636417dfeaff29f7713679b97cb234fbde3c7f98
CarineIzere <carizeree@gmail.com>
2019-02-12 12:13:46 +0200

    add assert

--- user_test.py
+++ user_test.py
@@ -1,5 +1,5 @@
-import unittest # Importing the unittest module
-from user import User # Importing the user class
+import unittest
+from user import User
 
 
 class TestUser(unittest.TestCase):
@@ -7,19 +7,13 @@ class TestUser(unittest.TestCase):
 
     def setUp(self):
         """Set Up Method to run before each test case to check if the class has been instantiated correctly"""
-        self.new_user = User("NewUser", "00000")
+        self.new_user = User("NewUser", "12345")
 
     def test_init(self):
         """Test to ensure that the object is initialized properly"""
-        self.assertEqual(self.new_user.Fname, "carine")
-        self.assertEqual(self.new_user.Lname, "Izere")
-        self.assertEqual(self.new_user.Gender, "Female")
-        self.assertEqual(self.new_user.Address, "Kigali")
-        self.assertEqual(self.new_user.e_mail, "carizeree@gmail.com")
-        self.assertEqual(self.new_user.Nationality, "Rwanda")
-        self.assertEqual(self.new_user.Status, "Married")
         self.assertEqual(self.new_user.user_name, "NewUser")
-        self.assertEqual(self.new_user.password, "00000")
+        self.assertEqual(self.new_user.password, "12345")
+
     def test_save_user(self):
         """Method that tests wether an user credentials have been successfully saved"""
         self.new_user.save_user()
