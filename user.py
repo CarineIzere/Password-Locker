ac39f76b47112920b7e0fa08e46f27824ba5e45d
CarineIzere <carizeree@gmail.com>
2019-02-12 12:08:07 +0200

    add methods that saves user objects into list

--- user.py
+++ user.py
@@ -1,27 +1,15 @@
 class User:
-    """Create class for users."""
+    """Create class for users"""
 
-        user_list = [] # Empty user list
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+    user_list = []
 
-        def __init__(self, Fname, Lname, Gender, Address, e_mail, Nationality, Status, user_name, password):
+    def save_user(self):
+        """Method that saves user objects into user_list"""
+        self.user_list.append(self)
 
-            # docstring removed for simplicity
-            self.Fname = Fname 
-            self.Lname = Lname
-            self.Gender = Gender 
-            self.Address = Address
-            self.e_mail = e_mail 
-            self.Nationality = Nationality
-            self.e_mail = e_mail 
-            self.user_name = user_name
-            self.password = password
-        
-        def save_user(self):
-            """Method that saves user objects into user_list"""
-            self.user_list.append(self)
-        def delete_user(self):
-            """Method that delete user objects into user_list"""
-            self.user_list.remove(self)
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
